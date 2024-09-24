a668da4b83feef7c993d66f05d534585f3fd7c55
Samidkun <samiddasproa@gmail.com>
2024-09-24 11:31:44 +0700

    JS05 - Pratikum 1

---
+++ JOBSHEET5/array_1.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+</head>
+
+<body>
+    <h2>Array Terindeks</h2>
+    <?php
+    $Listdosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+
+
+    foreach ($Listdosen as $dosen) {
+        echo $dosen . "<br>";
+    }
+    ?>
+</body>
+
+</html>
